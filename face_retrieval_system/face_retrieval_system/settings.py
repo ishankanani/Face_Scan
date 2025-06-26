79c35398462f59b9962f64ad40909f6e2b4fcc0c
Ishan Kanani <108275428+ishankanani@users.noreply.github.com>
2025-06-26 18:21:39 +0530

    Update settings.py

--- face_retrieval_system/face_retrieval_system/settings.py
+++ face_retrieval_system/face_retrieval_system/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-$1lb0bh1db+i#jbk2y%0(lmuriy(^wwp3t)3t&ahp7y=o)ezb)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
