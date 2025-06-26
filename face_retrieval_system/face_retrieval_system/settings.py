7418edb2e6d30d64a8454493d0707e9a7df238c6
Ishan Kanani <108275428+ishankanani@users.noreply.github.com>
2025-06-26 17:46:49 +0530

    Update settings.py

--- face_retrieval_system/face_retrieval_system/settings.py
+++ face_retrieval_system/face_retrieval_system/settings.py
@@ -52,12 +52,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'face_retrieval_system.urls'
 
